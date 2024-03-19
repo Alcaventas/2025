20515bb4153919adea2ba2318a9b6c494cd1435f
Alcaventas <141533784+Alcaventas@users.noreply.github.com>
2024-03-18 18:38:00 -0600

    Actualizar gc-link.js

--- plugins/gc-link.js
+++ plugins/gc-link.js
@@ -4,9 +4,8 @@ const handler = async (m, {conn, args}) => {
   conn.reply(m.chat, 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group), m, {
     contextInfo: {externalAdReply: {mediaUrl: null, mediaType: 1, description: null,
       title: '𝙻𝙸𝙽𝙺 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾',
-      body: '𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝',
-      previewType: 0, thumbnail: fs.readFileSync('./Menu2.jpg'),
-      sourceUrl: `https://github.com/BrunoSobrino/TheMystic-Bot-MD`}}});
+      body: '@alcashop.ff',
+      previewType: 0, thumbnail: fs.readFileSync('./alca.png
 };
 handler.help = ['linkgroup'];
 handler.tags = ['group'];
